35553cf5aa8e8c1aa03f6d573888faee0d62a0a5
bercikGz <bertas1122@gmail.com>
2024-02-19 12:29:54 +0100

    first commit

---
+++ Source/Snake1996/Snake1996.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Snake1996.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Snake1996, "Snake1996" );
